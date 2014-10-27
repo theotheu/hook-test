f20191272ceba0c48d48d45525929530a3bff616
Theo Theunissen <theo.theunissen@gmail.com>
2014-10-27 21:20:07 +0100

    pp

---
+++ hook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git commit -am "DEVELOPMENT: change to push 1) to development, 2) merge to test, 3) push to test"
+
+git push origin development
+
+git checkout test
+
+git merge development
+
+git commit -am "TEST: change to push 1) to development, 2) merge to test, 3) push to test"
+
+git push origin test
