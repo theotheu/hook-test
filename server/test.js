66cf001a65eff583b76580ba709e4c7a019f23ea
Theo Theunissen <theo.theunissen@gmail.com>
2014-10-28 20:52:38 +0100

    p

--- server/test.js
+++ server/test.js
@@ -5,38 +5,10 @@ function power(x) {
 
 console.log(power(2));
 
-<<<<<<< HEAD
-// now pushing to development
-=======
 
 power(3);
 
-<<<<<<< HEAD
-
-
-
-
-<<<<<<< HEAD
->>>>>>> development
-=======
-
-<<<<<<< HEAD
->>>>>>> development
-=======
-=======
->>>>>>> development
 // di 28 okt 2014 20:40:37 CET
-<<<<<<< HEAD
->>>>>>> development
-=======
 // di 28 okt 2014 20:42:33 CET
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> development
-=======
-// di 28 okt 2014 20:44:36 CET
->>>>>>> development
-=======
 
 // di 28 okt 2014 20:46:07 CET
->>>>>>> development
