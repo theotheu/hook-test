eedb1959e0a21fa0292e1c5bb162fa8ede26dd2b
Alt Admin <theotheu@server7-14-04.proxmox.tezzt.nl>
2014-10-31 22:13:59 +0100

    adding webhook

---
+++ webhook/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var app = express();
+var child;
+
+// Configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));     // Notice because option default will flip in next major; http://goo.gl/bXjyyz
+
+
+app.get('/webhook', function(req, res){
+  if( req.body.repository.url === 'https://github.com/theotheu/hook-test') {
+  	console.log('>>>>>req', req.body, '<<<<<<'); 
+	console.log('Now checkout the repo');
+  }
+  res.send({});
+});
+
+app.all('*', function(req, res) {
+	res.send({msg:'Nothing here. This is the webhook for github'});
+});
+app.listen(3333);
